13588332afa833144debd1311709ddd3d53fac40
Andreas Prell <andreas.h.prell@gmail.com>
2023-04-10 12:47:53 +0200

    Add tests

--- tests.ml
+++ tests.ml
@@ -12,6 +12,19 @@ let test_02 = {|
 
 let test_03 = {|
   inp w
+  add x w
+  eql w x
+|}
+
+let test_04 = {|
+  inp w
+  inp x
+  eql w x
+  eql w 2
+|}
+
+let test_05 = {|
+  inp w
   add z w
   mod z 2
   div w 2
@@ -24,11 +37,13 @@ let test_03 = {|
   mod w 2
 |}
 
-let test_04 = Alu.Utils.read_file_into_string "aoc_2021.txt"
+let test_06 = Alu.Utils.read_file_into_string "aoc_2021.txt"
 
 let tests = [
   test_01;
   test_02;
   test_03;
   test_04;
+  test_05;
+  test_06;
 ]
